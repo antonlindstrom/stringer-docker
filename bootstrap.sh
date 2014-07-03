d62449b69e4fe52a9dc94c3b11b795670ab932c3
Anton Lindström <lindztr@gmail.com>
2014-07-03 17:55:44 +0200

    update and cleanup bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,20 @@ docker run --name stringer_db -d postgres:9.3
 sleep 10
 export DB_PASSWORD=$(openssl rand -hex 20)
 
-create_user='exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -c "CREATE USER stringer WITH PASSWORD'
-password="'$DB_PASSWORD';\""
-create_database='exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -c "CREATE DATABASE stringer_live;"'
-grant_privileges='exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE stringer_live TO stringer;"'
+connect='exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U postgres -c '
 
-docker run --link stringer_db:postgres --rm postgres sh -c "${create_user} ${password}"
-docker run --link stringer_db:postgres --rm postgres sh -c $create_database
-docker run --link stringer_db:postgres --rm postgres sh -c $grant_privileges
+# SQL to create user and database
+create_user="CREATE USER stringer WITH PASSWORD '$DB_PASSWORD';"
+create_db="CREATE DATABASE stringerdb;"
+grant_priv="GRANT ALL PRIVILEGES ON DATABASE stringerdb TO stringer;"
+
+docker run --link stringer_db:postgres --rm postgres sh -c "${connect} \"${create_user}\";"
+docker run --link stringer_db:postgres --rm postgres sh -c "${connect} \"${create_db}\";"
+docker run --link stringer_db:postgres --rm postgres sh -c "${connect} \"${grant_priv}\";"
 
 docker run --name stringer_web \
 -e STRINGER_DATABASE_USERNAME="stringer" \
 -e STRINGER_DATABASE_PASSWORD="$DB_PASSWORD" \
+-e STRINGER_DATABASE="stringerdb" \
 -p 5000:5000 -d \
 --link stringer_db:postgres antonlindstrom/stringer
